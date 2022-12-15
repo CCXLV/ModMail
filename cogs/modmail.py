c5da6aa2a75930a68700a1df5a579b74e7838215
Giorgi Merebashvili <abracadabragio12@gmail.com>
2022-12-15 10:46:23 +0400

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -53,25 +53,25 @@ class ModMail(commands.Cog):
 
         if ctx.guild is None:
             channel = await guild.create_text_channel(name=ticket_name, category=category)
-                channel_id = get(guild.channels, name=str(channel))
+            channel_id = get(guild.channels, name=str(channel))
                
-                if channel_id:
-                    ch_id = channel_id.id
+            if channel_id:
+                ch_id = channel_id.id
 
-                em1 = discord.Embed(title="Staff channel created", description=f'Here is your channel <#{ch_id}>', color=0x04d277, timestamp=datetime.utcnow())
-                em1.set_thumbnail(url="https://user-images.githubusercontent.com/113610436/198890799-041e43ea-d7e9-46c1-bef1-c3180f24e0ef.png")
+            em1 = discord.Embed(title="Staff channel created", description=f'Here is your channel <#{ch_id}>', color=0x04d277, timestamp=datetime.utcnow())
+            em1.set_thumbnail(url="https://user-images.githubusercontent.com/113610436/198890799-041e43ea-d7e9-46c1-bef1-c3180f24e0ef.png")
                 
-                em2 = discord.Embed(title='ModMail channel', description='Please be patience, moderation staff will be there any time soon.\nTell us what do you need help about.', color=0x5e7bdd)
+            em2 = discord.Embed(title='ModMail channel', description='Please be patience, moderation staff will be there any time soon.\nTell us what do you need help about.', color=0x5e7bdd)
                 
                 
-                embed1 = await channel.send(embed=em2)
+            embed1 = await channel.send(embed=em2)
 
-                moderation_role = get(guild.roles, id=1024696145445924914)
-                await channel.send(message.author.mention + " " + moderation_role.mention)
-                await channel.set_permissions(message.author, 
-                                            view_channel=True,
-                                            read_messages=True)
-                await ctx.author.send(embed=em1)
+            moderation_role = get(guild.roles, id=1024696145445924914)
+            await channel.send(message.author.mention + " " + moderation_role.mention)
+            await channel.set_permissions(message.author, 
+                                          view_channel=True,
+                                          read_messages=True)
+            await ctx.author.send(embed=em1)
         elif:
             await ctx.send('DM ``help contact``')
     
